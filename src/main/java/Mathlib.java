bd1ccaa24f35a1c764ffee7d4083c94bc330e5cb
umair <cyumair@hotmail.com>
2022-12-08 16:35:40 +0500

    adding testcase

---
+++ src/main/java/Mathlib.java
@@ -0,0 +1,25 @@
+
+public class Mathlib {
+
+	
+	public Mathlib() {
+		
+	}
+	
+	public int add(int a, int b) {
+		return a + b;
+	}
+	
+	public int subtract(int a, int b) {
+		return a - b;
+	}
+	
+	public int multiply(int a, int b) {
+		return a * b;
+	}
+	
+	public int divide(int a, int b) {
+		return a / b;
+	}
+	
+}
---
+++ src/test/java/MathlibTest.java
@@ -0,0 +1,33 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class MathlibTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	
+	public void addTest() {
+		Mathlib obj = new Mathlib();
+		assert(10 == obj.add(6, 4));
+	}
+	
+	public void subTest() {
+		Mathlib obj = new Mathlib();
+		assert(10 == obj.subtract(15, 5));
+	}
+	
+	public void mulTest() {
+		Mathlib obj = new Mathlib();
+		assert(10 == obj.multiply(5, 5));
+	}
+	
+	public void divTest() {
+		Mathlib obj = new Mathlib();
+		assert(3 == obj.divide(15, 5));
+	}
+
+}
